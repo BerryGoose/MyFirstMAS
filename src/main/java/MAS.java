d412562dc1b2361f90f2438a1ab87bd2338a9071
Artem Kruglov <tema-kruglov@yandex.ru>
2022-07-05 14:07:18 +0300

    model classes

--- src/main/java/MAS.java
+++ src/main/java/MAS.java
@@ -1,12 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package MAS;
+import Geometry.*;
 
-/**
- *
- * @author Tema
- */
-public class MAS {
+
+class Route{
+    Point start;
+    Point end;
+    double length;
+    double direction;
     
+    int[] st_time = new int[2];
+    double cost;
+    int end_time;
+    String type;
+    Order order;
+}
+
+public class Courier{
+    String disp_addr;
+    Route[] routes;
+    Point position;
+    double direction;
+    int time;
+    double earned;
+    String status;
+    double satisfaction;
+}
+
+public class Order{
+    String disp_addr;
+    Point start;
+    Point end;
+    int[] st_time = new int[2];
+    int[] end_time = new int[2];
+    double max_cost;
+}
+
+public class Dispatcher{
+    Courier[] couriers;
+    Order[] orders;
+    int time;
+    double earned;
+    Order[] wait_list;
 }
