53175f7563189384d908cca13866289460983676
Artem Kruglov <tema-kruglov@yandex.ru>
2022-07-05 14:00:38 +0300

    Initial commit

---
+++ src/main/java/Geometry.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Tema
+ */
+public class Geometry {
+    double x = 0;
+    double y = 0;
+    double angle = 0;
+    
+}
