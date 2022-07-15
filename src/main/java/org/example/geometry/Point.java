8bbd2708d50f40fca94daeec2301a2bc7b5f82a5
Artem Kruglov <tema-kruglov@yandex.ru>
2022-07-15 10:31:54 +0300

    Made Point immutable

--- src/main/java/org/example/geometry/Point.java
+++ src/main/java/org/example/geometry/Point.java
@@ -3,8 +3,8 @@ package org.example.geometry;
 import java.util.Objects;
 
 public class Point {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
     public Point(){
         x = 0;
         y = 0;
@@ -64,9 +64,5 @@ public class Point {
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
-    }
-
-    public Point copy(){
-        return new Point(this.getX(), this.getY());
     }
 }
