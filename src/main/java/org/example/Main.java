c5bedfb3e39b0a73895bc1f883cae15d77aa9aef
Artem Kruglov <tema-kruglov@yandex.ru>
2022-07-21 13:02:09 +0300

    OrderReader refactor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,13 @@ package org.example;
 import org.example.actors.Order;
 import org.example.reader.OrderReader;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         String filePath = "Data\\orders.csv";
 
-        Order[] orderArr = OrderReader.readOrder(filePath);
+        List<Order> orderArr = OrderReader.readOrder(filePath);
         for (Order order: orderArr){
             System.out.println(order.toString());
         }
--- src/main/java/org/example/reader/OrderReader.java
+++ src/main/java/org/example/reader/OrderReader.java
@@ -5,51 +5,44 @@ import org.example.geometry.Point;
 import org.example.geometry.SimpleSegment;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class OrderReader {
-    static public Order[] readOrder(String filePath){
+    static public List<Order> readOrder(String filePath){
         return readOrder(filePath, 120, 480);
     }
 
-    static public Order[] readOrder(String filePath, int pickupWindow, int deliveryWindow){
+    static public List<Order> readOrder(String filePath, int pickupWindow, int deliveryWindow){
 
-        Order[] orderArray = new Order[0];
+        List<Order> orderArray = new ArrayList<>();
         try{
             File input = new File(filePath);
             FileReader inputReader = new FileReader(input);
             BufferedReader reader = new BufferedReader(inputReader);
-            try{
-                String line = reader.readLine();
+            String line = reader.readLine();
 
-                while(line != null){;
-                    String[] strs = line.split(", ");
-                    String[] inpStart = strs[1].split(" ");
-                    String[] inpEnd = strs[2].split(" ");
-                    String orderTime = strs[3];
+            while(line != null){;
+                String[] strs = line.split(", ");
+                String[] inpStart = strs[1].split(" ");
+                String[] inpEnd = strs[2].split(" ");
+                String orderTime = strs[3];
 
-                    double startTime = Double.parseDouble(orderTime);
+                double startTime = Double.parseDouble(orderTime);
 
-                    Point start = new Point(Double.parseDouble(inpStart[0]), Double.parseDouble(inpStart[1]));
-                    Point end = new Point(Double.parseDouble(inpEnd[0]),Double.parseDouble(inpEnd[1]));
-                    SimpleSegment startTimePeriod = new SimpleSegment(startTime, startTime + pickupWindow);
-                    SimpleSegment endTimePeriod = new SimpleSegment(startTime, startTime + deliveryWindow);
+                Point start = new Point(Double.parseDouble(inpStart[0]), Double.parseDouble(inpStart[1]));
+                Point end = new Point(Double.parseDouble(inpEnd[0]),Double.parseDouble(inpEnd[1]));
+                SimpleSegment startTimePeriod = new SimpleSegment(startTime, startTime + pickupWindow);
+                SimpleSegment endTimePeriod = new SimpleSegment(startTime, startTime + deliveryWindow);
 
-                    Order newOrder = new Order(start, end, startTimePeriod, endTimePeriod);
+                Order newOrder = new Order(start, end, startTimePeriod, endTimePeriod);
 
-                    Order[] tempOrderArray = new Order[orderArray.length + 1];
-
-                    System.arraycopy(orderArray, 0, tempOrderArray, 0, orderArray.length);
-                    tempOrderArray[orderArray.length] = newOrder;
-                    orderArray = tempOrderArray.clone();
-
-                    line = reader.readLine();
-                }
-            }catch (IOException ex){
-                ex.printStackTrace();
+                orderArray.add(newOrder);
+                line = reader.readLine();
             }
-        } catch (FileNotFoundException ex){
+        } catch (IOException ex){
             ex.printStackTrace();
-         }
+        }
         return orderArray;
     }
 }
